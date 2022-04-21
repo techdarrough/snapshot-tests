8cf1fd5708d478075c4aedb45ea0156bd608399e
Preston Darrough <techdarrough@gmail.com>
2022-04-21 14:28:10 -0700

    commit 2

--- src/CardMaker.test.js
+++ src/CardMaker.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import renderer from 'react-test-renderer'
-
+import CardMaker from './CardMaker'
 
 test('Renders a snapshot', () =>{
-  const tree = renderer.create(<App/>)
+  const tree = renderer.create(<CardMaker/>)
   expect(tree).toMatchSnapshot()
 })
